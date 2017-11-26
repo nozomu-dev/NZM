2406d114495c5c892c6c5e49d1e28535c2c8bf37
松下望 <matsu.uner@gmail.com>
2017-11-27 01:57:15 +0900

    WYSYWYGエディタ「quill」のインストール・gulpfileに必要ファイルの出力を設定

--- gulpfile.js
+++ gulpfile.js
@@ -5,24 +5,38 @@ var cssnext = require('postcss-cssnext');
 
 //それぞれの監視、コンパイル先のパスを定義
 var paths = {
-  'scss' : './scss/',
-  'css' : './doc_root/webroot/css/'
+    'scss' : './scss/',
+    'css' : './doc_root/webroot/css/',
+    'js' : './doc_root/webroot/js/'
 }
 
 //scssのコンパイル
 gulp.task('scss',function(){
-  var processors = [
-    cssnext()
-  ];
-   gulp.src(paths.scss + '**/*.scss')
-  .pipe(sass())
-  .pipe(postcss(processors))
-  .pipe(gulp.dest(paths.css));
+    var processors = [
+        cssnext()
+    ];
+    gulp.src(paths.scss + '**/*.scss')
+        .pipe(sass())
+        .pipe(postcss(processors))
+        .pipe(gulp.dest(paths.css));
 });
 
 //自動監視
 gulp.task('watch',['scss'],function(){
-  var scssWatcher = gulp.watch(paths.scss + '**/*.scss',['scss']);
-  scssWatcher.on('change',function(event){
-  });
+    var scssWatcher = gulp.watch(paths.scss + '**/*.scss',['scss']);
+        scssWatcher.on('change',function(event){
+    });
+});
+
+//ライブラリファイルの吐き出し
+gulp.task('quill', function(){
+    gulp.src([
+        'node_modules/quill/dist/quill.min.js',
+        'node_modules/quill/dist/quill.core.js',
+    ]).pipe(gulp.dest(paths.js + 'libs/'));
+
+    gulp.src([
+        'node_modules/quill/dist/quill.snow.css',
+        'node_modules/quill/dist/quill.core.css',
+    ]).pipe(gulp.dest(paths.css + 'libs/'));
 });
